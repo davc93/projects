e129aa9bd708f9e103c8dd978db4139048be5a43
Diego Vergara Casanova <davc93@gmail.com>
2022-10-30 14:25:52 -0300

    navbar exitosamente anadido

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,105 +1,123 @@
-import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
-import { context } from '../../context'
-import { logout } from '../../firebase/auth'
-import { Route } from '../../models'
-
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { context } from "../../context";
+import { logout } from "../../firebase/auth";
+import { Route } from "../../models";
+import './style.scss'
 const routes: Route[] = [
   {
-    name: 'About me',
-    path: '/',
+    name: "About me",
+    path: "/",
     private: false,
-    onlyPublic: false
+    onlyPublic: false,
   },
   {
-    name: 'Projects',
-    path: '/projects',
+    name: "Projects",
+    path: "/projects",
     private: false,
-    onlyPublic: false
+    onlyPublic: false,
   },
   {
-    name: 'Profile',
-    path: '/profile',
+    name: "Profile",
+    path: "/profile",
     private: true,
-    onlyPublic: false
+    onlyPublic: false,
   },
   {
-    name: 'Login',
-    path: '/login',
+    name: "Login",
+    path: "/login",
     private: false,
-    onlyPublic: true
-  }
-]
+    onlyPublic: true,
+  },
+];
 export const Navbar = () => {
-
-  const { isAuth,toggleDark }: any = useContext(context)
-  const [isActive, setIsActive] = React.useState(false)
+  const { isAuth, toggleDark }: any = useContext(context);
+  const [isActive, setIsActive] = React.useState(false);
 
   const handleLogout = () => {
-    logout()
-    setIsActive(!isActive)
-  }
+    logout();
+    setIsActive(!isActive);
+  };
   return (
-    <nav className='lg:flex w-full h-16 items-center justify-end px-8 fixed bg-gray-100'>
-
-      <div className=" menu-desktop">
-        <ul className='hidden lg:flex gap-2'>
-          {routes.map((route) => {
-            if (route.onlyPublic && isAuth) {
-              return null
-            }
-            if (route.private && !isAuth) {
-              return null
-            }
-            return (
-              <li key={route.path}>
-                <NavLink onClick={() => { setIsActive(!isActive) }} className='link' to={route.path} end>{route.name}</NavLink>
+    
+      <nav>
+        <div className="menu--desktop primary-navigation">
+          <div className="desktop-logo--container">
+            <img className="desktop-logo" src='' alt='Logo' />
+          </div>
+          <ul className="hidden lg:flex gap-2">
+            {routes.map((route) => {
+              if (route.onlyPublic && isAuth) {
+                return null;
+              }
+              if (route.private && !isAuth) {
+                return null;
+              }
+              return (
+                <li key={route.path}>
+                  <NavLink
+                    onClick={() => {
+                      setIsActive(!isActive);
+                    }}
+                    className="link"
+                    to={route.path}
+                    end
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              );
+            })}
+            {isAuth && (
+              <li>
+                <button onClick={handleLogout} className="link">
+                  Logout
+                </button>
               </li>
-            )
-
-          })}
-          {isAuth &&
+            )}
             <li>
-              <button onClick={handleLogout} className='link'>
-                Logout
-              </button>
+              <button onClick={toggleDark}>Dark Mode</button>
             </li>
-          }
-          <li>
-            <button onClick={toggleDark}>Dark Mode</button>
-          </li>
-        </ul>
-      </div>
-      <div className='menu-icon lg:hidden '>
-        <button onClick={() => { setIsActive(!isActive) }}>
-          <svg className="w-12 h-12 absolute right-0 top-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
-        </button>
-      </div>
-      {isActive && 
-      <div className="menu-mobile">
-        <ul className='lg:hidden  flex flex-col fixed pt-20 border-2 border-r-indigo-800 items-center gap-2 top-0 left-0 bg-gray-200 h-screen w-1/2'>
-          {routes.map((route) => {
-            if (route.onlyPublic && isAuth) {
-              return null
-            }
-            if (route.private && !isAuth) {
-              return null
-            }
-            return (
-              <li key={route.path}>
-                <NavLink onClick={() => { setIsActive(!isActive) }} className='link' to={route.path} end>{route.name}</NavLink>
-              </li>
-            )
-
-          })}
-          {isAuth &&
-            <button onClick={handleLogout} className='link'>
-              Logout
-            </button>}
-
-        </ul>
-      </div>
-      }
-    </nav>
-  )
-}
+          </ul>
+        </div>
+        <div className="menu--mobile">
+          <div className="container nav-container">
+            <input className="checkbox" type="checkbox" />
+            <div className="hamburger-lines">
+              <span className="line line1" />
+              <span className="line line2" />
+              <span className="line line3" />
+            </div>
+            <div className="logo">
+              <img src='' alt='' />
+            </div>
+            <div className="menu-items">
+            {routes.map((route) => {
+              if (route.onlyPublic && isAuth) {
+                return null;
+              }
+              if (route.private && !isAuth) {
+                return null;
+              }
+              return (
+                <li key={route.path}>
+                  <NavLink
+                    onClick={() => {
+                      setIsActive(!isActive);
+                    }}
+                    className="link"
+                    to={route.path}
+                    end
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              );
+            })}
+            </div>
+          </div>
+        </div>
+      </nav>
+    
+  );
+};

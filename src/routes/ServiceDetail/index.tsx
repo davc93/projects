ad784bbd89f4d44653afd88f9cfd095a54ea8a34
Diego Vergara Casanova <davc93@gmail.com>
2022-11-17 19:19:52 -0300

    se integran servicios

--- src/routes/ServiceDetail/index.tsx
+++ src/routes/ServiceDetail/index.tsx
@@ -4,18 +4,27 @@ import { context } from '../../context'
 import { Service } from '../../models'
 
 export const ServiceDetail = () => {
+  const handleClick = () => {
+    window.location.href = 'mailto:davc93@gmail.com'
+  }
   const { services }:any = useContext(context)
   const { slug } = useParams()
   const service: Service = services.find((service: Service) => service.name.replaceAll(' ', '-').toLowerCase() == slug)
+  
   if (service) {
-
+    const priceFormated = new Intl.NumberFormat('en-US',{
+      style:'currency',
+      currency:'CLP'
+    })
     return (
-      <header>
-        <article>
+      <header className='mx-6 md:mx-24 mt-8'>
+        <article className='flex flex-col gap-4 md:w-2/3'>
           <h1>{service.name}</h1>
           <p>{service.description}</p>
-          <span>{service.price}</span>
+          <span>{ priceFormated.format(service.price) } / Per section</span>
           {service.discount && (<span>{service.price * (1 - service.discount)}</span>)}
+          <p>Each section has the size of the monitor display</p>
+          <button onClick={handleClick} className='btn btn--primary'>Contactar</button>
         </article>
       </header>
     )
--- src/routes/ServicesPage/index.tsx
+++ src/routes/ServicesPage/index.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export const ServicesPage = () => {
   return (
     <>
-      <header className="relative h-screen mx-4">
+      <header className="relative h-screen md:h-96 mx-4">
         <div className="hero-container flex flex-col items-center absolute justify-center inset-x-0 top-24 text-center gap-4">
           <h1 className="hero__title">Find the service that you need</h1>
           <h3 className="hero__subtitle">Personalized services to what you need</h3>
         </div>
       </header>
-      <main className="mx-6">
-        <section className="landing-page mb-6 lg:mb-16">
+      <main className="mx-6 md:mx-32">
+        <section className="landing-page h-96 mb-6 lg:mb-16 md:w-2/3">
           <article className="flex flex-col gap-4">
             <h1>Landing Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta molestias obcaecati odio illo mollitia excepturi saepe natus labore ipsum enim ut porro modi commodi deserunt, consequatur ad eveniet aliquam cumque?</p>
@@ -19,11 +19,11 @@ export const ServicesPage = () => {
           </article>
           <img src="" alt="" />
         </section>
-        <section className="web-site">
-        <article className="flex flex-col gap-4">
+        <section className="web-site  h-96 md:flex md:justify-end">
+        <article className="flex flex-col md:w-2/3 gap-4 md:text-end">
             <h1>Web Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, obcaecati nemo. Tenetur, magni laboriosam voluptates quia alias exercitationem dolor atque velit illum quis </p>
-            <Link to={'/service/web-site'} className="btn btn--primary">Create web page</Link>
+            <Link to={'/service/web-site'} className=" md:self-end btn btn--primary">Create web page</Link>
           </article>
           <img src="" alt="" />
 

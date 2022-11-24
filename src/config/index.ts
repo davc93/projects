6ece193c9a89aab2223a39afc4ff3778e998c6c2
Diego Vergara Casanova <davc93@gmail.com>
2022-11-24 12:09:02 -0300

    [fix] api key typo

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
 
-    API_KEY:import.meta.env.VITE_apiKey,
+    API_KEY:import.meta.env.VITE_API_KEY,
     MP_SERVER:import.meta.env.VITE_MP_SERVER ?? 'https://mercadopago.onrender.com',
     MP_PUBLIC_KEY:import.meta.env.VITE_MP_PUBLIC_KEY,
 

5f44266f5b950c198b4aaae2a3a0cc0093c915d3
Diego Vergara Casanova <davc93@gmail.com>
2022-11-29 11:07:36 -0300

    se traspasa backend a otro repo

--- api/projects/index.js
+++
@@ -1,9 +0,0 @@
-import db from '../../db'
-export default function handler(request, response) {
-    response.status(200).json({
-        body: {
-            name:'Diego',
-            age:29
-        },
-    });
-  }
